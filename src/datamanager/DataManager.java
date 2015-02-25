c56f8e8e4a5264df8e66778ef6294548ca69cfc3
charles <charles@charles-VirtualBox>
2015-02-25 18:31:47 -0300

    Essencia da base de dados sendo gerada

--- src/datamanager/DataManager.java
+++ src/datamanager/DataManager.java
@@ -26,7 +26,6 @@ import java.util.logging.Logger;
 public class DataManager {
 
     public Map<String, Double> features = new HashMap<>();
-    public List<String> palavrasEssenciais = new ArrayList<>();
     public double min, max;
 
     /**
@@ -35,18 +34,30 @@ public class DataManager {
     public static void main(String[] args) {
 
         DataManager dm = new DataManager();
-        //dm.geraEsssenciaBaseDados();
-        dm.getMinMaxFrequency("1Gram.csv");
+        dm.geraEsssenciaBaseDados("word_frequency.csv", "texto.csv");
         System.out.println(dm.min + " : " + dm.max + " : ");
         System.out.println("done");
     }
 
-    public void geraEsssenciaBaseDados() {
+    public void geraEsssenciaBaseDados(String wordFrequencFile, String databaseFile) {
         System.out.println("teste");
-
         try {
-            lerDadosWordRank("word_frequency.csv");
-            printFile("test.csv", "");
+            lerDadosWordRank(wordFrequencFile);
+            getMinMaxFrequency(databaseFile);
+            List<Feature> features = generateRankedDataBase(databaseFile);
+            for (Feature feature : features) {
+                System.out.println(feature.getNome() + " : " + feature.getRank());
+            }
+            System.out.println("*******************************************************");
+            features.sort(new FeatureComparator());
+            System.out.println("*******************************************************");
+            StringBuilder sb = new StringBuilder();
+            for (Feature feature : features) {
+                System.out.println(feature.getNome() + " : " + feature.getRank());
+                sb.append(feature.getNome() + " : " + feature.getRank());
+                sb.append("\n");
+            }
+          printFile("test2.csv", sb.toString());
         } catch (Exception ex) {
             System.out.println("Erro manipulacao de arquivo");
         }
@@ -63,57 +74,33 @@ public class DataManager {
                 if (linha.contains("Rank")) {
                     continue;
                 }
-                palavra = linha.split(",")[0] + " : " + linha.split(",")[1];
+                palavra =  linha.split(",")[1];
                 rank = Double.parseDouble(linha.split(",")[5]);
                 features.put(palavra, rank);
             }
         }
+        System.out.println(features.size());
     }
 
     private void printFile(String fileName, String texto) throws IOException {
-        try {
-            StringBuilder sb = new StringBuilder();
-            try (FileWriter fw = new FileWriter(fileName); BufferedWriter bw = new BufferedWriter(fw)) {
-                for (String key : features.keySet()) {
-                    sb.append("\n");
-                    sb.append(key);
-                    sb.append(":");
-                    sb.append(features.get(key));
-                }
-                bw.write(sb.toString());
-            }
-        } catch (IOException ex) {
-            System.out.println("Erro manipulacao de dados...");
-        }
-    }
 
-    private void createEssentialDataBase(String filePath) {
-        String palavra;
-        String linha;
-        double rank;
-        try (FileReader fr = new FileReader(filePath); BufferedReader br = new BufferedReader(fr)) {
-            while (br.ready()) {
-                linha = br.readLine();
-                palavra = linha.split(",")[2];
-                rank = Double.parseDouble(linha.split(",")[0]);
-                if (features.containsKey(palavra)) {
-                    palavrasEssenciais.add(palavra + ", " + features.get(palavra));
-                }
-                features.put(palavra, rank);
-            }
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(DataManager.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(DataManager.class.getName()).log(Level.SEVERE, null, ex);
+        StringBuilder sb = new StringBuilder();
+        try (FileWriter fw = new FileWriter(fileName); BufferedWriter bw = new BufferedWriter(fw)) {
+//            for (String key : features.keySet()) {
+//                sb.append("\n");
+//                sb.append(key);
+//                sb.append(":");
+//                sb.append(features.get(key));
+//            }
+           
+            bw.write(texto);
         }
-
     }
 
     public void getMinMaxFrequency(String filePath) {
         String palavra;
         String linha;
         double frequencia = 0;
-
         try (FileReader fr = new FileReader(filePath); BufferedReader br = new BufferedReader(fr)) {
             String primeiraLinha = br.readLine();
             min = Double.parseDouble(primeiraLinha.split(":")[0]);
@@ -138,30 +125,31 @@ public class DataManager {
         String linha;
         double rank;
         double essencia;
-        List<Feature> essencias = new ArrayList<>();
-
+        List<Feature> palavrasEssenciais = new ArrayList<>();
         try (FileReader fr = new FileReader(filePath); BufferedReader br = new BufferedReader(fr)) {
             while (br.ready()) {
                 linha = br.readLine();
                 if (linha.length() < 2) {
                     continue;
                 }
-                palavra = linha.split(",")[2];
-                rank = Double.parseDouble(linha.split(",")[0]);
+                palavra = linha.split(":")[2];
+                rank = Double.parseDouble(linha.split(":")[0]);
+                //normalizacao do dado de 0 ~ 1
                 rank = (rank - min) / (max - min);
                 if (features.containsKey(palavra)) {
                     essencia = rank - features.get(palavra);
                 } else {
+                    System.out.println("Nao achei " + palavra);
                     essencia = 0;
                 }
-                essencias.add(new Feature(palavra, rank));
+                palavrasEssenciais.add(new Feature(palavra, essencia));
             }
         } catch (FileNotFoundException ex) {
             Logger.getLogger(DataManager.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(DataManager.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return essencias;
+        return palavrasEssenciais;
     }
 
 }
