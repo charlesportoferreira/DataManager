ec4fe77e70d619680af3436382f822cc0f7e8ab0
charles <charles.ferreira@aluno.ufabc.edu.br>
2015-03-19 20:34:49 -0300

    removido importacao nao utilizada

--- src/datamanager/DataManager.java
+++ src/datamanager/DataManager.java
@@ -18,9 +18,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Map.Entry;
-import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
